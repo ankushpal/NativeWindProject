31a37f33a876b0435522c660c973cf331ee5dfb9
Ankush Pal <ankushpal@Ankushs-MacBook-Pro.local>
2025-06-23 23:29:37 +0400

    Initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    // presets: ['module:metro-react-native-babel-preset', 'nativewind/babel'],
+    presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+    plugins: ['react-native-reanimated/plugin'],
+  };
 };
---
+++ src/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Loggin = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-bold text-blue-600">Hello NativeWind</Text>
+    </View>
+  );
+}
+
+export default Loggin
